4eca29d65e0d2ca06380e7d70a0645590d875da7
Social Codia <socialcodia@gmail.com>
2020-05-01 01:29:35 +0530

    in LoginActivity system ui has been init.

--- app/src/main/java/com/socialcodia/socialshopia/LoginActivity.java
+++ app/src/main/java/com/socialcodia/socialshopia/LoginActivity.java
@@ -3,12 +3,29 @@ package com.socialcodia.socialshopia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //UI
+    private EditText inputEmail, inputPassword;
+    private Button btnLogin;
+    private TextView tvRegister, tvForgotPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //UI init
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvRegister = findViewById(R.id.tvRegister);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+
     }
 }
