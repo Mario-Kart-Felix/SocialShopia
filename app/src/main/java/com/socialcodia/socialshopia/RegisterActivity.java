02d17195bf589307bf0b34aa1024ef6e511f370d
Social Codia <socialcodia@gmail.com>
2020-05-01 02:25:43 +0530

    RegisterActivity designed by Umair Farooqui.

--- app/src/main/java/com/socialcodia/socialshopia/RegisterActivity.java
+++ app/src/main/java/com/socialcodia/socialshopia/RegisterActivity.java
@@ -3,6 +3,13 @@ package com.socialcodia.socialshopia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +17,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
 }
