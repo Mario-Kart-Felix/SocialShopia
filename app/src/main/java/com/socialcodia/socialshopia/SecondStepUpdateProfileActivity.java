f7814ba8a8dd59a44660374fa66b42054e5fc6d4
Social Codia <socialcodia@gmail.com>
2020-05-01 04:55:44 +0530

    Created SecondStepUpdateProfileActivity

---
+++ app/src/main/java/com/socialcodia/socialshopia/SecondStepUpdateProfileActivity.java
@@ -0,0 +1,14 @@
+package com.socialcodia.socialshopia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondStepUpdateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_step_update_profile);
+    }
+}
