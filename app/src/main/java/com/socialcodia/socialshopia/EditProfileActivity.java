fff80a8bfb93531c6d7621335af5347b2ad34915
Social Codia <socialcodia@gmail.com>
2020-05-02 02:45:38 +0530

    Edit Profile Activity UI Designed by Umair Farooqui.

--- app/src/main/java/com/socialcodia/socialshopia/EditProfileActivity.java
+++ app/src/main/java/com/socialcodia/socialshopia/EditProfileActivity.java
@@ -1,8 +1,24 @@
 package com.socialcodia.socialshopia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.socialcodia.socialshopia.storage.Constants;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -10,5 +26,6 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
     }
 }
