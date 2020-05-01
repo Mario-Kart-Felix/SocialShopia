23ca3b90f4e6719c2064af2ef9dc8d6d1f5574c7
Social Codia <socialcodia@gmail.com>
2020-05-02 02:49:36 +0530

    Bugs Fix

--- app/src/main/java/com/socialcodia/socialshopia/EditProfileActivity.java
+++ app/src/main/java/com/socialcodia/socialshopia/EditProfileActivity.java
@@ -22,10 +22,64 @@ import com.socialcodia.socialshopia.storage.Constants;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    private ImageButton btnBack, btnLocation;
+    private ImageView userProfileImage;
+    private EditText inputName, inputMobileNumber, inputCity, inputState, inputCountry, inputAddress;
+    private Button btnUpdateProfile;
+
+    //Firebase
+    FirebaseAuth mAuth;
+    FirebaseDatabase mDatabase;
+    DatabaseReference mRef;
+    FirebaseUser mUser;
+
+    String userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        btnBack = findViewById(R.id.btnBack);
+        btnLocation = findViewById(R.id.btnLocation);
+        userProfileImage = findViewById(R.id.userProfileImage);
+        inputName = findViewById(R.id.inputName);
+        inputMobileNumber = findViewById(R.id.inputMobileNumber);
+        inputCity = findViewById(R.id.inputCity);
+        inputState = findViewById(R.id.inputState);
+        inputCountry = findViewById(R.id.inputCountry);
+        inputAddress = findViewById(R.id.inputAddress);
+        btnUpdateProfile = findViewById(R.id.btnUpdateProfile);
+
+        //Firebase Init
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference();
+        mUser = mAuth.getCurrentUser();
+
+        if (mUser!=null)
+        {
+            userId = mUser.getUid();
+        }
+
+        getUserData();
+    }
+
+    private void getUserData()
+    {
+        mRef.child(Constants.USERS).child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child(Constants.USER_NAME).getValue(String.class);
+                Toast.makeText(EditProfileActivity.this, "", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
